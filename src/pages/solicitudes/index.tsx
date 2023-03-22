798bc8a2693f39ea8bbeab3aee895bcb8254328f
Brandon Parrillas <brandonps.dev@gmail.com>
2023-03-22 10:23:13 -0600

    sorting table funcionando

--- src/pages/solicitudes/index.tsx
+++ src/pages/solicitudes/index.tsx
@@ -1,61 +1,12 @@
-//MUI
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
-import { Chip } from "@mui/material";
-//
-import { stableSort, getComparator, Order } from "@/src/utils/tableUtils";
-import EncabezadoTabla, { HeadCell } from "@/src/components/table/Encabezado";
 import { useEffect, useState } from "react";
 import { trpc } from "@/src/utils/trpc";
 import { useAlert } from "@/src/hooks/alertStore";
+import SortingTable from '@/src/components/table/SortingTable';
+import type { Encabezado } from '@/src/components/table/Encabezado';
 
-interface Data {
-  calories: number;
-  carbs: number;
-  fat: number;
-  name: string;
-  protein: number;
-}
-
-const headCells: HeadCell[] = [
-  {
-    id: "fechaDeSolicitud",
-    numeric: false,
-    disablePadding: true,
-    label: "Fecha",
-  },
-  {
-    id: "nombreDelSolicitante",
-    numeric: false,
-    disablePadding: false,
-    label: "Nombre",
-  },
-  {
-    id: "institucionSolicitante",
-    numeric: false,
-    disablePadding: false,
-    label: "Institución",
-  },
-  {
-    id: "estado.id",
-    numeric: false,
-    disablePadding: false,
-    label: "Estado",
-  },
-  {
-    id: "notas",
-    numeric: false,
-    disablePadding: false,
-    label: "Notas",
-  },
-];
+const headers: 
 
 export default function Solicitudes() {
-  const [order, setOrder] = useState<Order>("asc");
-  const [orderBy, setOrderBy] = useState<string>("calories");
   const { lanzarAlerta } = useAlert();
   const solicitudQuery = trpc.solicitud.lista.useQuery();
 
@@ -64,76 +15,13 @@ export default function Solicitudes() {
       lanzarAlerta(solicitudQuery.error.message, { severity: "error" });
   }, [solicitudQuery.isError]);
 
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: string
-  ) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
+  const formatSolicitudes =()=> {
+    let data = []
 
-  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {};
+    if(solicitudQuery.data?.lista){
+      return data
+    }
+  }
 
-  return (
-    <TableContainer>
-      <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
-        <EncabezadoTabla
-          headCells={headCells}
-          order={order}
-          orderBy={orderBy}
-          onRequestSort={handleRequestSort}
-          rowCount={
-            solicitudQuery.data?.lista ? solicitudQuery.data.lista.length : 0
-          }
-        />
-        {solicitudQuery.data === undefined ? (
-          <TableBody></TableBody>
-        ) : (
-          <TableBody>
-            {stableSort(
-              solicitudQuery.data.lista,
-              getComparator(order, orderBy)
-            ).map((row: typeof solicitudQuery.data.lista[0], index) => {
-              const labelId = `enhanced-table-checkbox-${index}`;
-
-              return (
-                <TableRow
-                  hover
-                  onClick={(event) =>
-                    handleClick(event, row.nombreDelSolicitante)
-                  }
-                  role="checkbox"
-                  tabIndex={-1}
-                  key={`row-solicitud-${row.id}`}
-                >
-                  <TableCell
-                    component="th"
-                    id={labelId}
-                    scope="row"
-                    padding="none"
-                  >
-                    {row.fechaDeSolicitud}
-                  </TableCell>
-                  <TableCell align="right">
-                    {`${row.nombreDelSolicitante} ${row.apellidoDelSolicitante}`}
-                  </TableCell>
-                  <TableCell align="right">
-                    {row.institucionSolicitante}
-                  </TableCell>
-                  <TableCell align="right">
-                    <Chip
-                      label={row.estado.nombre}
-                      color={row.estado.id === 2 ? "success" : "default"}
-                    />
-                  </TableCell>
-                  <TableCell align="right">{row.notas}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        )}
-      </Table>
-    </TableContainer>
-  );
+  return <SortingTable />;
 }
