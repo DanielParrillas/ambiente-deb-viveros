514000b7b2fc03822dfbadb2d2d880796ddefa8a
Brandon Parrillas <brandonps.dev@gmail.com>
2023-03-22 08:33:02 -0600

    TablaFuncionando

--- src/components/table/ExampleSAST.tsx
+++ src/components/table/ExampleSAST.tsx
@@ -23,12 +23,14 @@ function createData(
   };
 }
 
+interface DataCell {
+  value: string | number;
+  content: string | JSX.Element;
+}
+
 const rows = [
   createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Cupcdfsake", 305, 3.7, 67, 4.3),
 ];
 
 const headCells = [
